b77a854475a10843c53da36808514ebd18535da5
shwetap29 <shwetaprasad48@gmail.com>
2021-05-24 00:34:22 +0530

    ADDED: Welcome message

---
+++ src/main/java/com/brideglabz/LineComparisonProblem.java
@@ -0,0 +1,7 @@
+package com.brideglabz;
+
+public class LineComparisonProblem {
+      public static void main(String[] args) {
+          System.out.println("Welcome to Line Comparison Problem");
+      }
+}
